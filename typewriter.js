1497410717984414599915103a8393f10615934d
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-02 18:44:02 +0000

    changed variable name for loop

--- typewriter.js
+++ typewriter.js
@@ -2,11 +2,11 @@ const sentence = "hello there from lightouse labs";
 
 const typewriter = function(string) {
 
-  for (let delay = 0; delay < string.length; delay++) {
-    
+  for (let i = 0; i < string.length; i++) {
+
       setTimeout(() => {
-        process.stdout.write(string[delay]);
-      }, delay * 50)
+        process.stdout.write(string[i]);
+      }, i * 50)
   }
 
   setTimeout(() => {
